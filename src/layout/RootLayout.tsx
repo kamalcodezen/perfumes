aa1b73edb0fd20b4130736c807d226def003c778
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-11 14:39:11 +0530

    change root layout

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/common/Navbar/Navbar";
 
-
 const RootLayout = () => {
   return (
     <>
-      <Navbar/>
-      <Outlet />
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+
+      <footer>Footer</footer>
     </>
   );
 };
