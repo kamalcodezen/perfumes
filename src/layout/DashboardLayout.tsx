507edfd2427fa4c9c913103cba31e1566c6ebfbc
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-13 14:19:34 +0530

    Dashboard navabr remove

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,21 +1,15 @@
 import { Outlet } from "react-router-dom";
 import DashboardSidebar from "../components/dashboard/DashboardSidebar";
-import DashboardNavbar from "../components/dashboard/DashboardNavbar";
 
 const DashboardLayout = () => {
   return (
     <div className="min-h-screen flex flex-col lg:flex-row">
       <DashboardSidebar />
 
-      <div className="min-h-screen flex-1">
-        {/* top navbar */}
-        <DashboardNavbar />
-
-        {/* page content */}
-        <main className="min-h-screen ">
-          <Outlet />
-        </main>
-      </div>
+      {/* page content */}
+      <main className="min-h-screen ">
+        <Outlet />
+      </main>
     </div>
   );
 };
