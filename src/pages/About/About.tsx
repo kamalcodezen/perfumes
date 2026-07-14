61ed73df086b683127aef7f627af06c9b0c278e9
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-14 12:45:42 +0530

    feat(about): create about page

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,20 @@
+import AboutHero from "../../components/about/AboutHero/AboutHero";
+import BrandStory from "../../components/about/BrandStory/BrandStory";
+import OurMission from "../../components/about/OurMission/OurMission";
+import OurValues from "../../components/about/OurValues/OurValues";
+
+const About = () => {
+  return (
+    <>
+      <AboutHero />
+
+      <BrandStory />
+
+      <OurMission />
+
+      <OurValues />
+    </>
+  );
+};
+
+export default About;
