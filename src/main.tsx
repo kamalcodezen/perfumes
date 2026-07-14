13148d360e41afa6d3a8974ec9ff525d90936c33
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-14 23:09:03 +0530

    chore(app): configure TanStack Query provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,25 @@ import { router } from "./routes/router";
 import ThemeProvider from "./context/ThemeProvider";
 import { ToastContainer } from "react-toastify";
 
+//  Import QueryClient and QueryClientProvider
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a QueryClient instance
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-      <ToastContainer />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+        <ToastContainer />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
