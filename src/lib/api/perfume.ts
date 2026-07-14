00fbedb67acfaa110270c1377c6238c09782cd07
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-14 23:06:28 +0530

    feat(perfume): add backend pagination support

--- src/lib/api/perfume.ts
+++ src/lib/api/perfume.ts
@@ -1,12 +1,11 @@
 import { serverFetch } from "../core/server";
 
-// all perfumes get
-export const getPerfumes = async () => {
-  return await serverFetch("/api/perfumes");
+// all perfumes get (with Pagination / TanStack Query support)
+export const getPerfumes = async ({pageParam = 1,}: { pageParam?: number } = {}) => {
+  return await serverFetch(`/api/perfumes?page=${pageParam}&limit=8`);
 };
 
 // perfumes get by id
 export const getPerfumesById = async (id: string) => {
   return await serverFetch(`/api/perfumes/${id}`);
 };
-
