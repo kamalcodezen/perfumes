4c7a8d4b957dc1b187190daf4b7c3c10ab653c83
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-15 15:48:15 +0530

    feat: add AOS fade-up entrance animation to Contact page layout

--- src/pages/Contact/ContactPage.tsx
+++ src/pages/Contact/ContactPage.tsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import ContactForm from "../../components/contact/ContactForm";
 import ContactHero from "../../components/contact/ContactHero";
 import ContactInfo from "../../components/contact/ContactInfo";
 
 const ContactPage = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+      once: true,
+      easing: "ease-out-cubic",
+    });
+  }, []);
+
   return (
-    <main className="w-full">
+    <main className="w-full overflow-x-hidden min-h-screen" data-aos="fade-up">
       <ContactHero />
       <ContactInfo />
       <ContactForm />
